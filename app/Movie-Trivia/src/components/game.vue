f858e5b4f8a1edff0e7014f5c39d18cd38b67998
Conor Hooley <conorhooley@Conor-Hooleys-MacBook-Pro.local>
2018-04-27 15:19:18 -0600

    Committing styled .vue files

--- app/Movie-Trivia/src/components/game.vue
+++ app/Movie-Trivia/src/components/game.vue
@@ -58,36 +58,130 @@ export default {
 </script>
 
 <style scoped>
-  img {
-    display: none;
-  }
-  .colContainer{
-   display: flex;
-   flex-flow: row;
-   align-items: center;
-   justify-content: space-around;
-   padding: 20px;
-  }
+* {
+    background-color: grey;
+    font-family: 'Raleway', sans-serif;
+    text-align: center;
+   }
 
-  .col {
-   border: 1px black solid;
-  }
+header {
+padding: 15px;
+}
 
-  div {
-   border: 1px black dotted;
-  }
+h2 {
+font-size: 50px;
+}
 
-  .div1 {
-   padding: 10px;
-   margin: 10px;
-   height: 100px;
-   width: 300px;
-  }
+.colContainer{
+display: flex;
+flex-flow: row;
+align-items: center;
+justify-content: space-around;
+padding: 20px;
+}
 
-  .div2 {
-   padding: 10px;
-   margin: 10px;
-   height: 600px;
-   width: 300px;
-  }
+.col {
+border: 1px black solid;
+}
+
+div {
+border: 1px black dotted;
+}
+
+.div1 {
+padding: 10px;
+margin: 10px;
+height: 100px;
+width: 300px;
+}
+
+.div2 {
+padding: 10px;
+margin: 10px;
+height: 600px;
+width: 300px;
+}
+
+.navbar {
+display: flex;
+justify-content: space-around;
+}
+
+.aboutContainer {
+display: flex;
+flex-flow: row;
+justify-content: space-around;
+}
+
+.staffInfo {
+display: flex;
+flex-flow: column;
+align-items: center;
+padding: 20px;
+}
+
+.staffTag {
+padding: 10px;
+font-family: 'Fjalla One', sans-serif;
+}
+
+.subtitle {
+font-style: italic;
+}
+
+.infoContainer {
+display: flex;
+flex-flow: column;
+}
+
+ol {
+list-style-type: none;
+}
+
+li {
+padding: 20px;
+}
+
+.footerBox {
+float: right;
+padding: 20px;
+}
+
+i {
+padding: 4px;
+margin: 0px 5px;
+}
+
+i:hover {
+color: yellow;
+}
+
+.fa {
+font-size: 40px;
+}
+
+.staffInfo img {
+    width: 200px;
+}
+
+.gameScreenshot {
+    display: flex;
+    justify-content: space-around;
+    margin: 40px;
+}
+
+
+.brochureTxt {
+    display: flex;
+    justify-content: center;
+    padding: 80px;
+    margin: 40px;
+}
+
+.statsTxt {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+}
 </style>
--- app/Movie-Trivia/src/components/header.vue
+++ app/Movie-Trivia/src/components/header.vue
@@ -9,8 +9,132 @@ export default {
   name: 'HelloWorld'
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+* {
+    background-color: grey;
+    font-family: 'Raleway', sans-serif;
+    text-align: center;
+   }
+
+header {
+padding: 15px;
+}
+
+h2 {
+font-size: 50px;
+}
+
+.colContainer{
+display: flex;
+flex-flow: row;
+align-items: center;
+justify-content: space-around;
+padding: 20px;
+}
+
+.col {
+border: 1px black solid;
+}
+
+div {
+border: 1px black dotted;
+}
+
+.div1 {
+padding: 10px;
+margin: 10px;
+height: 100px;
+width: 300px;
+}
+
+.div2 {
+padding: 10px;
+margin: 10px;
+height: 600px;
+width: 300px;
+}
+
+.navbar {
+display: flex;
+justify-content: space-around;
+}
+
+.aboutContainer {
+display: flex;
+flex-flow: row;
+justify-content: space-around;
+}
+
+.staffInfo {
+display: flex;
+flex-flow: column;
+align-items: center;
+padding: 20px;
+}
+
+.staffTag {
+padding: 10px;
+font-family: 'Fjalla One', sans-serif;
+}
+
+.subtitle {
+font-style: italic;
+}
+
+.infoContainer {
+display: flex;
+flex-flow: column;
+}
+
+ol {
+list-style-type: none;
+}
+
+li {
+padding: 20px;
+}
+
+.footerBox {
+float: right;
+padding: 20px;
+}
+
+i {
+padding: 4px;
+margin: 0px 5px;
+}
+
+i:hover {
+color: yellow;
+}
+
+.fa {
+font-size: 40px;
+}
+
+.staffInfo img {
+    width: 200px;
+}
+
+.gameScreenshot {
+    display: flex;
+    justify-content: space-around;
+    margin: 40px;
+}
+
+
+.brochureTxt {
+    display: flex;
+    justify-content: center;
+    padding: 80px;
+    margin: 40px;
+}
+
+.statsTxt {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+}
 </style>
