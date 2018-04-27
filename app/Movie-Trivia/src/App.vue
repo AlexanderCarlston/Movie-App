86f8ae3dce18d8ee0b92ecf31b1e5947b05bbd39
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-04-27 13:21:47 -0600

    got vue structure working on app

--- app/Movie-Trivia/src/App.vue
+++ app/Movie-Trivia/src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <TheHeader/>
+    <TheGame :TheGame="TheGame"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TheHeader from "./components/header.vue"
+import TheGame from "./components/game.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader,
+    TheGame
+  },
+  data(){
+  return {
+    TheGame: {}
   }
+},
+  mounted(){
+      fetch("http://www.omdbapi.com/?i=tt3896198&apikey=28fb5af8")
+      .then(response => {
+        return response.json()
+      })
+      .then(response => {
+        this.TheGame = response
+      })
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ app/Movie-Trivia/src/components/game.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <div class="colContainer">
+      <div class="col">
+        <div class="div1">{{TheGame.Plot}}</div>
+        <div id = "first" class="div2">
+          <ul>
+            <li>{{TheGame.Runtime}}</li>
+            <li>{{TheGame.Directo}}</li>
+            <li>{{TheGame.Actors}}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col">
+        <div class="div1">{{TheGame.Year}}</div>
+        <div id = "second" class="div2">
+          <img :src='TheGame.Poster'>
+        </div>
+      </div>
+      <div class="col">
+        <div class="div1">{{TheGame.Genre}}</div>
+        <div id = "third" class="div2">
+          <ol>
+            <li>Blade Runner</li>
+            <li>Fifth Element</li>
+            <li>Guardians of the Galaxy</li>
+            <li>Star Wars: Return of the Jedi</li>
+            <li>Total Recall</li>
+            <li>Avatar</li>
+            <li>Galaxy Quest</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  props: ["TheGame"]
+  }
+</script>
+
+<style scoped>
+
+</style>
